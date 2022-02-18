c91e8daa201408e35b0e3861f52c2b27d2a18404
Dragoslav Tamindzija <dragodev@protonmail.com>
2022-02-18 12:05:06 +0100

    getting urls

--- scrapers/csgocases.go
+++ scrapers/csgocases.go
@@ -14,16 +14,18 @@ import (
 	_ "github.com/lib/pq"
 )
 
-var BASE_URL string
+var BASE_URL_TWITTER string
 var BEARER_TOKEN string
 var CSGOCASES_ID string = "943452686820761600"
 var DB_NAME_WEBSITES string
+var BASE_URL_OCR string
+var OCR_API_KEY string
 
 type meta struct{
     Oldest_id string `json:"oldest_id"`
     Newest_id string `json:"newest_id"`
-    Result_count int `result_count:"result_count"`
-    Next_token string `next_token:"next_token"`
+    Result_count int `json:"result_count"`
+    Next_token string `json:"next_token"`
 }
 
 type individualTweet struct{
@@ -34,6 +36,24 @@ type individualTweet struct{
 type resBody struct{
     Meta meta `json:"meta"`
     Data []individualTweet `json:"data"`
+}
+
+type tweetInfo struct{
+    Data struct{
+        Attachments struct{
+            Media_keys []string `json:"media_keys"`
+        }                       `json:"attachments"`
+        Id string               `json:"id"`
+        Text string             `json:"text"`
+    }                           `json:"data"`
+
+    Includes struct{
+        Media []struct{
+            Media_key string    `json:"media_key"`
+            Type string         `json:"type"`
+            Url string          `json:"url"`
+        }                       `json:"media"`
+    }                           `json:"includes"`
 }
 
 func getlastOldestId(db *sql.DB) (error, string) {
@@ -49,15 +69,77 @@ func getlastOldestId(db *sql.DB) (error, string) {
     }
 }
 
+func getCodes(tweets []string) (error, []string){
+    var imageURLs []string
+    var codes []string
+    client := http.Client{}
+
+    // grabbing urls
+    for _, tweet := range tweets{
+        req, err := http.NewRequest("get", BASE_URL_TWITTER + "tweets/" + tweet + "?media.fields=url&expansions=attachments.media_keys", nil)
+        if err != nil{
+            return err, nil
+        }
+
+        req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", BEARER_TOKEN))
+        res, err := client.Do(req)
+        if err != nil{
+            return err, nil
+        }
+
+        body, err := ioutil.ReadAll(res.Body)
+
+        if err != nil{
+            return err, nil
+        }
+
+        var resJSON tweetInfo
+        err = json.Unmarshal([]byte(body), &resJSON)
+
+        if err != nil{
+            return err, nil
+        }
+        imageURLs = append(imageURLs, resJSON.Includes.Media[0].Url)
+    }
+
+    // passing images through ocr
+    for _, url := range imageURLs{
+        req, err := http.NewRequest("get", BASE_URL_OCR + fmt.Sprintf("url?apiKey=%s&url=%s",OCR_API_KEY, url), nil)
+        if err != nil{
+            return err, nil
+        }
+
+        res, err := client.Do(req)
+        if err != nil{
+            return err, nil
+        }
+
+        body, err := ioutil.ReadAll(res.Body)
+        if err != nil{
+            return err, nil
+        }
+
+        log.Print(string(body))
+        log.Print(url)
+        log.Println("")
+
+    }
+
+    return nil, codes
+}
+
 func Scrape(db *sql.DB) error {
     err := godotenv.Load(".env")
     if err != nil{
         log.Print("Cannot load .env for scraper!")
         return err
     }
-    BASE_URL = os.Getenv("TWITTER_BASE_URL")
+    BASE_URL_TWITTER = os.Getenv("TWITTER_BASE_URL")
     BEARER_TOKEN = os.Getenv("BEARER_TOKEN")
     DB_NAME_WEBSITES = os.Getenv("DB_NAME_WEBSITES")
+    BASE_URL_OCR = os.Getenv("OCR_BASE_URL")
+    OCR_API_KEY = os.Getenv("OCR_API_KEY")
+
     err, oldOldestId  := getlastOldestId(db)
 
     if err != nil{
@@ -67,7 +149,7 @@ func Scrape(db *sql.DB) error {
     // fetching tweets
     client := http.Client{}
 
-    req, err := http.NewRequest("get", BASE_URL + "users/" + CSGOCASES_ID + "/tweets?max_results=20", nil)
+    req, err := http.NewRequest("get", BASE_URL_TWITTER + "users/" + CSGOCASES_ID + "/tweets?max_results=20", nil)
     if err != nil{
         log.Print("Problem with making a new request!")
         return err
@@ -103,6 +185,7 @@ func Scrape(db *sql.DB) error {
 
     // going through new tweets
     var tweets []individualTweet = resJSON.Data
+    var tweetsIds []string
 
     for _, tweet := range tweets{
         if tweet.Id <= oldOldestId{
@@ -110,8 +193,14 @@ func Scrape(db *sql.DB) error {
         }
 
         if strings.Contains(tweet.Text, "promocode will get free $"){
-            log.Print(tweet.Text)
+            tweetsIds = append(tweetsIds, tweet.Id)
         }
+    }
+
+    err, _ = getCodes(tweetsIds)
+
+    if err != nil {
+        return err
     }
 
     return nil
